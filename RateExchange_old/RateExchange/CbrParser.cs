136031675d65f70333c5c0be74d03ff0c5812a94
Twix298 <48965303+Twix298@users.noreply.github.com>
2019-09-19 10:16:47 +0300

    Add files via upload

---
+++ RateExchange_old/RateExchange/CbrParser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml;
+using System.Data.SqlClient;
+
+namespace RateExchange
+{
+    class CbrParser
+    {
+
+        public static void ReaderCbr(string link)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(link);
+            XmlNodeList nodeListCharCode = doc.GetElementsByTagName("CharCode");
+            XmlNodeList elemListValues = doc.GetElementsByTagName("Value");
+            XmlElement root = doc.DocumentElement;
+            string date = root.GetAttribute("Date");
+
+            Console.WriteLine(date);
+            for (int i =0; i < elemListValues.Count;i++)
+            {
+                Console.WriteLine(elemListValues[i].InnerText);
+                Console.WriteLine(nodeListCharCode[i].InnerText);
+            }
+            //поменять AttachDbFilename на директорию где будет находиться проект
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\vs_project\RateExchange_old\RateExchange\DatabaseCbr.mdf; Integrated Security = True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string sql = string.Format("INSERT INTO Course" + "(date, currency, value) VALUES(@date, @currency, @value)");
+
+                for (int i = 0; i < nodeListCharCode.Count; i++)
+                {
+                    using (SqlCommand cmd = new SqlCommand(sql, connection))
+                    {
+                        cmd.Parameters.Add("@date", Convert.ToDateTime(date));
+                        cmd.Parameters.Add("@currency", nodeListCharCode[i].InnerText);
+                        cmd.Parameters.Add("@value", (float)Convert.ToDouble(elemListValues[i].InnerText));
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            
+
+            }
+        }
+    }
+}
---
+++ RateExchange_old/RateExchange/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RateExchange
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string link = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=21.08.2019";
+            CbrParser.ReaderCbr(link);
+            label1.Text = "Готово";
+
+        }
+    }
+}
